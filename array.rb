218549f5cd4395510dac35bc8f4f15fba26d9ba6
Alan Kwong <kwong.alan@yahoo.com>
2019-10-27 01:09:06 +0000

    image blur 1

--- array.rb
+++ array.rb
@@ -1,16 +1,24 @@
 class Image
 
-  def initialize
-   @image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0],
-])
-   image.output_image
-  end
+   def initialize(image)
+    @image = image
+   end
 
-  def output_image
-      puts @image
+   def output_image
+    puts @image.join
+    
+      @image.each do |image|
+      puts @image.join
     end
+   end
 end
+
+    image = Image.new([
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 1],
+    [0, 0, 0, 0],
+    ])
+    image.output_image
+
+    
